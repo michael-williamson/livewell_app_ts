f9428b041e925e718126fcbcdc35db555b72dd1c
michael-williamson <michaeldavid1721@yahoo.com>
2020-12-17 19:47:43 -0600

    initial auth0 setup

---
+++ src/auth0-provider-with-history.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode } from "react";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { customHistory } from "./history/history";
+import { keys } from "./keys/keys";
+
+interface IProps {
+  children: ReactNode;
+}
+
+export const Auth0ProviderWithHistory = ({ children }: IProps) => {
+  const onRedirectCallback = (appState: any) => {
+    customHistory.push(appState?.returnTo || window.location.pathname);
+  };
+
+  return (
+    <Auth0Provider
+      domain={keys.domain}
+      clientId={keys.clientId}
+      redirectUri="http://localhost:3000/"
+      onRedirectCallback={onRedirectCallback}
+      audience={keys.audience}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};

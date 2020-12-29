d114f0c65d00504df24fedd15cfc16738a832bec
michael-williamson <michaeldavid1721@yahoo.com>
2020-12-28 19:26:46 -0600

    added FishView component and adjusted Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,23 +3,74 @@ import {
   AppBar,
   Button,
   IconButton,
+  MenuItem,
   Toolbar,
   Typography,
+  Menu,
+  makeStyles,
+  createStyles,
+  Theme,
 } from "@material-ui/core";
-import { Menu } from "@material-ui/icons";
+import { Menu as MenuIcon } from "@material-ui/icons";
 
-function Header() {
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  })
+);
+
+const Header = () => {
+  const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <Menu />
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+          className={classes.menuButton}
+        >
+          <MenuIcon />
         </IconButton>
-        <Typography variant="h6">Livewell App</Typography>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>Profile</MenuItem>
+          <MenuItem onClick={handleClose}>My account</MenuItem>
+          <MenuItem onClick={handleClose}>Logout</MenuItem>
+        </Menu>
+        <Typography variant="h6" className={classes.title}>
+          Livewell App
+        </Typography>
         <Button color="inherit">Login</Button>
       </Toolbar>
     </AppBar>
   );
-}
+};
 
 export default Header;
---
+++ src/components/tableComponents/FishView.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import {
+  Button,
+  createStyles,
+  makeStyles,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Theme,
+  withStyles,
+} from "@material-ui/core";
+import FishDeleteModal from "./FishDeleteModal";
+
+const StyledTableCell = withStyles((theme: Theme) =>
+  createStyles({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  })
+)(TableCell);
+
+const StyledTableRow = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      "&:nth-of-type(odd)": {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  })
+)(TableRow);
+
+function createData(name: string, calories: number, fat: number) {
+  return { name, calories, fat };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0),
+  createData("Ice cream sandwich", 237, 9.0),
+  createData("Eclair", 262, 16.0),
+  createData("Cupcake", 305, 3.73),
+  createData("Gingerbread", 356, 16.0),
+];
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: "white",
+    width: "100%",
+    marginTop: 40,
+    marginBottom: 40,
+    textAlign: "center",
+  },
+  table: {
+    minWidth: 700,
+  },
+});
+
+const FishView = () => {
+  const classes = useStyles();
+  return (
+    <TableContainer component={Paper} className={classes.root}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
+            <StyledTableCell align="right">Calories</StyledTableCell>
+            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
+            <StyledTableCell align="right"></StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.calories}</StyledTableCell>
+              <StyledTableCell align="right">{row.fat}</StyledTableCell>
+              <StyledTableCell align="right">
+                <FishDeleteModal />
+              </StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default FishView;

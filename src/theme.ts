299ccebca50bdf54d6cdc73070d5dc00e9546de0
michael-williamson <michaeldavid1721@yahoo.com>
2020-12-28 19:25:25 -0600

    adjusted styling for app and index

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,5 @@
-import { red } from "@material-ui/core/colors";
 import { createMuiTheme } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
 
 // A custom theme for this app
 const theme = createMuiTheme({
@@ -12,6 +12,9 @@ const theme = createMuiTheme({
     },
     error: {
       main: red.A400,
+    },
+    info: {
+      main: "#fff",
     },
     background: {
       default: "#fff",

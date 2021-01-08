bf79fd088a37d39390f908068509b76a86873aaf
michael-williamson <michaeldavid1721@yahoo.com>
2021-01-07 20:22:37 -0600

    added styles to signin button

--- src/components/authComponents/SignInButton.tsx
+++ src/components/authComponents/SignInButton.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button } from "@material-ui/core";
+import { Button, createStyles, makeStyles, Theme } from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    button: {
+      backgroundColor: "yellow",
+      color: "forestgreen",
+    },
+  })
+);
 
 const SignInButton = () => {
   const { loginWithRedirect } = useAuth0();
+  const classes = useStyles();
   return (
-    <Button variant="contained" onClick={() => loginWithRedirect()}>
+    <Button
+      variant="contained"
+      onClick={() => loginWithRedirect()}
+      className={classes.button}
+    >
       Sign In
     </Button>
   );

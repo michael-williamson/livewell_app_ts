31550fd1663d324af50b3acd1cc8efcb3477bc7f
michael-williamson <michaeldavid1721@yahoo.com>
2021-01-12 21:19:36 -0600

    added styles to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  makeStyles,
+} from "@material-ui/core";
 import { AddCircleOutline, LocationOn, TableChart } from "@material-ui/icons";
 
+const useStyles = makeStyles({
+  root: {
+    height: 60,
+    marginTop: 42,
+  },
+});
+
 const Footer = () => {
+  const classes = useStyles();
   return (
-    <BottomNavigation showLabels>
+    <BottomNavigation showLabels className={classes.root}>
       <BottomNavigationAction label="View Fish" icon={<TableChart />} />
       <BottomNavigationAction label="Add Fish" icon={<AddCircleOutline />} />
       <BottomNavigationAction label="Nearby" icon={<LocationOn />} />

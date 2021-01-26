11b38d8eef036c44c80b2cc8b21eef15bdf7cacb
michael-williamson <michaeldavid1721@yahoo.com>
2021-01-26 11:25:45 -0600

    created random messages for TackleBox comp

---
+++ src/components/tackleBoxComponents/dialog.ts
@@ -0,0 +1,62 @@
+export const dialog = [
+  {
+    d1: "How's that sun? You staying cool?",
+    d2: "Largemouth bass have been known to like some shade",
+    i1: "",
+    i2: "",
+  },
+  {
+    d1: "Where is that mosquito repellant",
+    d2: "If I had dollar for every bug bite",
+    i1: "",
+    i2: "",
+  },
+  {
+    d1: "Much wind today?",
+    d2: "That wind can really be something",
+    i1: "",
+    i2: "",
+  },
+  {
+    d1: "Water birds are fishing too.",
+    d2: "Maybe they can share.",
+    i1: "",
+    i2: "",
+  },
+  {
+    d1: "A country mile ain't that bad.",
+    d2: "Easy does it",
+    i1: "",
+    i2: "",
+  },
+  {
+    d1: "How many boats are out?",
+    d2: "Anybody got the right idea?",
+    i1: "",
+    i2: "",
+  },
+  {
+    d1: "Lures come and go.",
+    d2: "We've all lost a few",
+    i1: "",
+    i2: "",
+  },
+  {
+    d1: "Did you put the plug in?",
+    d2: "Just checking,  it happens from time to time",
+    i1: "",
+    i2: "",
+  },
+  {
+    d1: "Blue skies are sure easy on the eyes",
+    d2: "Fishing is a way of life",
+    i1: "",
+    i2: "",
+  },
+  {
+    d1: "That's a nice boat you've got there",
+    d2: "How bout we catch some fish",
+    i1: "",
+    i2: "",
+  },
+];

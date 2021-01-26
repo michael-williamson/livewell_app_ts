193768a4c86b3f3cc5fc579e33622919c2e92a40
michael-williamson <michaeldavid1721@yahoo.com>
2021-01-26 11:23:08 -0600

    wired up TackleBox comp to Redux

--- src/components/tackleBoxComponents/TackleBox.tsx
+++ src/components/tackleBoxComponents/TackleBox.tsx
@@ -1,30 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
-import { createStyles, Grid, makeStyles, Theme } from "@material-ui/core";
+import {
+  Card,
+  CardContent,
+  createStyles,
+  Grid,
+  makeStyles,
+  Theme,
+  Typography,
+} from "@material-ui/core";
 import logo from "../../media/livewell_fish_logo.png";
 import tacklebox from "../../media/tacklebox.jpg";
+import { useDispatch } from "react-redux";
+import { getUserFish } from "../../actions";
+import {
+  AddCircleOutline,
+  Cloud,
+  TableChart,
+  WbSunny,
+} from "@material-ui/icons";
+import { dialog } from "./dialog";
+import Switch from "../helperComponents/Switch";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
+      minHeight: "80vh",
+    },
+    grid: {
       marginTop: 30,
       marginBottom: 100,
       borderRadius: 5,
       backgroundImage: `url(${tacklebox})`,
       backgroundSize: "cover",
       backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
       position: "relative",
       zIndex: 1,
       boxShadow: "9px 11px 16px #ffff0012",
-      border: "2px solid #ffff0024",
+      // border: "2px solid #ffff0024",
+      border: "1px solid white",
       "&::after": {
         content: "''",
         borderRadius: 5,
         position: "absolute",
         top: 0,
         left: 0,
-        background: "rgba(0,0,0,0.72)",
+        // background: "rgba(0,0,0,0.72)",
+        background: "rgb(0 168 195 / 20%)",
         width: "100%",
         height: "100%",
         zIndex: -1,
@@ -37,17 +61,54 @@ const useStyles = makeStyles((theme: Theme) =>
     button: {
       background: theme.palette.primary.contrastText,
       boxShadow: `7px 8px 8px 0px #ffff0036`,
+      border: "1px solid #f4ff0091",
     },
     route: {
       color: theme.palette.primary.main,
     },
+    card: {
+      margin: "10px 8px 57px",
+      textAlign: "center",
+      backgroundImage: "linear-gradient(45deg, #5eff541f, #ffffff40)",
+      boxShadow: "inset 1px 1px 20px 2px #81b11e4a",
+      background: "#5b822e94",
+      border: "2px solid #5bff0063",
+      "& .MuiTypography-root": {
+        fontSize: 21,
+        textShadow: "1px 1px #327132",
+        fontFamily: "cursive",
+        color: "#fff704",
+        lineHeight: "40px",
+      },
+    },
+    switchGridItem: {
+      textAlign: "center",
+      color: theme.palette.secondary.main,
+    },
   })
 );
 
-export const TackleBox = () => {
-  const classes = useStyles();
+const dialogShuffler = (array: any) => {
+  let len = array.length;
+  let randomIndex = Math.floor(Math.random() * len);
   return (
     <div>
+      <Typography>{array[randomIndex].d1}</Typography>
+      <Typography>{array[randomIndex].d2}</Typography>
+    </div>
+  );
+};
+
+export const TackleBox = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUserFish());
+  }, [dispatch]);
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
       <img
         src={logo}
         alt="fish"
@@ -55,32 +116,49 @@ export const TackleBox = () => {
           width: 314,
           height: 155,
           padding: "0px 10px 0px",
-          marginTop: 20,
+          margin: "20px auto 0",
+          display: "block",
         }}
       />
-      <Grid
-        alignContent="center"
-        justify="space-around"
-        className={classes.root}
-      >
+      <Grid className={classes.grid}>
         <Grid item className={classes.gridItem}>
-          <Button variant="contained" className={classes.button} size="large">
+          <Button
+            variant="contained"
+            className={classes.button}
+            size="large"
+            endIcon={<TableChart color="primary" />}
+          >
             <Link to="/user/fish-view" className={classes.route}>
               View Fish
             </Link>
           </Button>
         </Grid>
         <Grid item className={classes.gridItem}>
-          {" "}
-          <Button variant="contained" className={classes.button} size="large">
+          <Button
+            variant="contained"
+            className={classes.button}
+            size="large"
+            endIcon={<AddCircleOutline color="primary" />}
+          >
             <Link to="/user/fish-enter" className={classes.route}>
               Add Fish
             </Link>
           </Button>
         </Grid>
+        <Grid item>
+          <Card className={classes.card}>
+            <CardContent>
+              <WbSunny color="primary" fontSize="large" />
+              {dialogShuffler(dialog)}
+              <Cloud color="secondary" fontSize="large" />
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item className={classes.switchGridItem}>
+          <Switch />
+          <label>turn off dialog</label>
+        </Grid>
       </Grid>
     </div>
   );
 };
-
-export default TackleBox;

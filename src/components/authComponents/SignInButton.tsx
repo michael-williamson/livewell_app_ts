095c53758c99e1659af7b6edfe972a2f2ccadc4a
michael-williamson <michaeldavid1721@yahoo.com>
2021-02-02 21:38:29 -0600

    added functionality to signin button

--- src/components/authComponents/SignInButton.tsx
+++ src/components/authComponents/SignInButton.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button, createStyles, makeStyles, Theme } from "@material-ui/core";
+import { LockOpen } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     button: {
       backgroundColor: "yellow",
       color: theme.palette.primary.contrastText,
+      fontWeight: 800,
+      "& .MuiSvgIcon-root": {
+        fill: "#3a3a3a",
+      },
     },
   })
 );
@@ -20,6 +25,7 @@ const SignInButton = () => {
       variant="contained"
       onClick={() => loginWithRedirect()}
       className={classes.button}
+      endIcon={<LockOpen />}
     >
       Sign In
     </Button>
